4fc1a509a6b05e98534f7159eb7af97194ef9169
Rohit Agrawal <rohit_agrawal1@persistent.co.in>
2019-08-29 10:53:29 +0530

    Modified Models wrt ROS2

--- launch/small_house.launch.py
+++ launch/small_house.launch.py
@@ -31,26 +31,36 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import ThisLaunchFileDir
 from launch.actions import ExecuteProcess
 from launch.substitutions import LaunchConfiguration
+# Added for gazebo_ros -- spawn entity node 
+from launch_ros.actions import Node
 
 import lifecycle_msgs.msg
 
 
 def generate_launch_description():
-    gui = LaunchConfiguration('gui', default='false')
+    gui = LaunchConfiguration('gui', default='true')
     paused = LaunchConfiguration('paused', default='false')
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     headless = LaunchConfiguration('headless', default='false')
     debug = LaunchConfiguration('debug', default='false')
     verbose = LaunchConfiguration('verbose', default='true')
-
+    
     world_file_name = 'small_house.world'
     package_dir = get_package_share_directory('aws_robomaker_small_house_world')
     world = os.path.join(package_dir, 'worlds', world_file_name)
-
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_simulation_description'), 'models','turtlebot3_waffle/model.sdf')
+    # GAZEBO_MODEL_PATH has to be correctly set for Gazebo to be able to find the model
+    # ros2 run gazebo_ros spawn_entity.py -entity myentity -x 7.5 -y 1.2 -z 0 -file src/turtlebot3_simulation_description/models/turtlebot3_waffle/model.sdf 
+    spawn_entity = Node(package='gazebo_ros', node_executable='spawn_entity.py',
+                        arguments=['-entity', 'myentity', '-x','6.5','-y','-0.3','-z','0','-file',launch_file_dir],
+                        output='screen')
+    
     return LaunchDescription([       
         ExecuteProcess(
-            cmd=['gzserver', world, '-s', 'libgazebo_ros_init.so','libgazebo_ros_state.so', gui, paused, use_sim_time, headless, debug, verbose],
+            # cmd=['gazebo', world, '-s', 'libgazebo_ros_init.so','libgazebo_ros_state.so','libgazebo_ros_factory.so', gui, paused, use_sim_time, headless, debug, verbose],
+            cmd=['gazebo', world, '-s','libgazebo_ros_factory.so','libgazebo_ros_init.so','libgazebo_ros_state.so', gui, paused, use_sim_time, headless, debug, verbose],
             output='screen'),
+        spawn_entity,
     ])
 
 if __name__ == '__main__':
